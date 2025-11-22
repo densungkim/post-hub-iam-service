1d5dcfd98880db1b216e56eb0d56b5b806838cd4
Denis Kim <densungkim@gmail.com>
2025-11-22 17:21:16 +0500

    chore(tests): remove unused @TestInstance annotations from service test classes

--- src/test/java/com/post/hub/iamservice/unit/service/PostServiceTest.java
+++ src/test/java/com/post/hub/iamservice/unit/service/PostServiceTest.java
@@ -21,7 +21,6 @@ import com.post.hub.iamservice.utils.ApiUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -43,7 +42,6 @@ import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 @Tag("unit")
 class PostServiceTest {
 
